19b134411f988496cc9bbe2fa0f259a3aec7c5e3
Hanok Lamsal <hanoklamsal@gmail.com>
2025-05-14 22:08:16 +0530

    tab switch fix

--- script.js
+++ script.js
@@ -94,3 +94,23 @@ document.addEventListener('DOMContentLoaded', function () {
     modal.hide(); 
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const tabButtons = document.querySelectorAll('.tab-button');
+  const tabContents = document.querySelectorAll('.content');
+
+  tabButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      tabButtons.forEach((btn) => btn.classList.remove('active'));
+      button.classList.add('active');      const targetId = button.getAttribute('data-id');
+      tabContents.forEach((content) => content.classList.remove('active'));
+
+      const targetContent = document.getElementById(targetId);
+      if (targetContent) {
+        targetContent.classList.add('active');
+      }
+    });
+  });
+});
